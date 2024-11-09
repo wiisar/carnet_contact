376af3f67c3ec1a5416233d367f938f866ddd0dc
Benjamin MANEM <99032433+wiisar@users.noreply.github.com>
2024-11-09 17:14:53 +0100

    Add sexe enum to replace char sexe

--- main.c
+++ main.c
@@ -1,14 +1,14 @@
 #include<stdio.h>
 
 
-enum sexe {H,F};
+typedef enum {H,F}sexe;
 struct contact_details
 {
   int id;
   char surname[50];
   char name[50];
   char address[50];
-  char sexe[1];
+  sexe typesexe;
   char telnumber[10];
 };
 
@@ -27,20 +27,32 @@ struct carnet_address{
 */
 
 void contact_afficher(const struct contact_details* contact){
-  printf("Le contact est :\n Nom : %s \n Prénom : %s \n Adresse : %s \n Sexe : %s \n Tel : %s ",contact->surname, contact->name, contact->address, contact->sexe, contact->telnumber);
+  printf("Le contact est :\n Nom : %s \n Prénom : %s \n Adresse : %s \n Sexe : %s \n Tel : %s\n ",contact->surname, contact->name, contact->address, contact->typesexe == H ? "Homme" : "Femme", contact->telnumber);
 }
 
 
 struct contact_details contact_saisir(struct contact_details* contact){
   contact->id =  12;
+
+  char sexe_temp[2];
+
   printf("Saisir le nom du contact : ");
   scanf("%s", contact->surname);
+
   printf("Saisir le prénom du contact : ");
   scanf("%s", contact->name);
+
   printf("Saisir l'adresse du contact : "); //fgets pour les chaines de caractére avec des espaces
   scanf("%s", contact->address);
   printf("Saisir le sexe du contact (H/F) : ");
-  scanf("%s", contact->sexe);
+  scanf("%s", sexe_temp);
+  if (sexe_temp[0] == 'H' || sexe_temp[0] == 'h') {
+    contact->typesexe = H;
+  } 
+  else if (sexe_temp[0] == 'F' || sexe_temp[0] == 'f') {
+    contact->typesexe = F;
+  }
+
   printf("Saisir le numéro de téléphone du contact : ");
   scanf("%s", contact->telnumber);
 
