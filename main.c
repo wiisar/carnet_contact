493fede465727709a3c55368b9cd31294d393ed1
Benjamin MANEM <99032433+wiisar@users.noreply.github.com>
2024-11-15 11:59:51 +0100

    Cours 15/11

--- main.c
+++ main.c
@@ -1,7 +1,10 @@
 #include<stdio.h>
 
+#define MAX_CONTACTS 100
 
 typedef enum {H,F}sexe;
+
+
 struct contact_details
 {
   int id;
@@ -10,29 +13,38 @@ struct contact_details
   char address[50];
   sexe typesexe;
   char telnumber[10];
+  struct contact_details *next;
 };
 
-/*
+
 
 struct contact{
   struct contact *next;
-  struct contact_details value;
-
+  struct contact_details *value;
 };
 
-struct carnet_address{
-  struct contact *head;
-
+struct pool{
+  struct contact *available_head;
+  struct contact *used_head;
 };
-*/
+
 
 void contact_afficher(const struct contact_details* contact){
-  printf("Le contact est :\n Nom : %s \n Prénom : %s \n Adresse : %s \n Sexe : %s \n Tel : %s\n ",contact->surname, contact->name, contact->address, contact->typesexe == H ? "Homme" : "Femme", contact->telnumber);
+  printf("Le contact est :\n Nom : %s \n Prénom : %s \n Adresse : %s \n Sexe : %s \n Tel : %s\n ID : %d ",contact->surname, contact->name, contact->address, contact->typesexe == H ? "Homme" : "Femme", contact->telnumber,contact->id); 
+}
+void pool_switch(struct contact* contact_add, struct contact *contact_head){
+  if (contact_add == NULL|| contact_head == NULL){
+    return ;
+  }
+  contact_add->next = contact_head->next;
+  contact_head->next = contact_add;
 }
 
 
-struct contact_details contact_saisir(struct contact_details* contact){
-  contact->id =  12;
+void contact_saisir(struct contact_details* contact, struct pool *carnet){
+  if (contact == NULL || carnet == NULL){
+    return;
+  }
 
   char sexe_temp[2];
 
@@ -44,6 +56,7 @@ struct contact_details contact_saisir(struct contact_details* contact){
 
   printf("Saisir l'adresse du contact : "); //fgets pour les chaines de caractére avec des espaces
   scanf("%s", contact->address);
+  //fgets(contact->address, sizeof(int), stdin);
   printf("Saisir le sexe du contact (H/F) : ");
   scanf("%s", sexe_temp);
   if (sexe_temp[0] == 'H' || sexe_temp[0] == 'h') {
@@ -52,19 +65,25 @@ struct contact_details contact_saisir(struct contact_details* contact){
   else if (sexe_temp[0] == 'F' || sexe_temp[0] == 'f') {
     contact->typesexe = F;
   }
-
   printf("Saisir le numéro de téléphone du contact : ");
   scanf("%s", contact->telnumber);
 
-  return *contact;
+  //pool_switch(contact,carnet);
 
 }
 
 
 int main(int argc, char *argv[]){
-  struct contact_details c1;
-  contact_saisir(&c1);
-  contact_afficher(&c1);
+  struct contact_details pool_available[MAX_CONTACTS]={};
+
+  struct contact_details cd1;
+  struct contact c1 = {NULL, &cd1};
+  struct pool carnet = {&c1,NULL};
+
+  //struct pool Used;
+  contact_saisir(&cd1, &carnet);
+  contact_afficher(carnet.available_head->value);
+  
   return 0;
 
 }
